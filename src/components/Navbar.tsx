0bda308834736c2ea108625ba2864109f711f991
Himanshu Tiwari <htiwari578@gmail.com>
2025-03-31 17:17:09 +0530

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { getKindeServerSession, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server"
+import MaxWithWrapper from "./common/MaxWithWrapper";
+import Link from "next/link";
+import { Headphones } from "lucide-react";
+import { buttonVariants } from "./ui/button";
+
+
+const Navbar = async () => {
+  const {getUser} = getKindeServerSession();
+  const user = await getUser();
+  return (
+    <MaxWithWrapper className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <nav className="container flex h-16 items-center justify-between">
+      <div className="flex items-center gap-">
+      <Link  
+      href="/"
+      className="flex items-center gap-2 text-lg font-semibold">
+
+      <Headphones className="h-6 w-6" />
+      <span>Listen-AI PRO</span>
+
+      </Link>
+      </div>
+      <div className="flex items-center gap-4">
+        <div className="">
+
+          {
+            user ? (
+              <>
+              <Link href="/dashbord/quizzes">Quizzes</Link>
+              <Link href="/dashbord">Dashboard</Link>
+              <LogoutLink  className={buttonVariants({
+                    variant: "secondary",
+                    size: "sm",
+                  })}>
+                    Logout
+              </LogoutLink>
+              </>
+            ) : (
+              <LoginLink  className={buttonVariants({ variant: "default", size: "sm" })}>
+                Login
+              </LoginLink>
+            )
+          }
+        </div>
+      </div>
+      </nav>
+    </MaxWithWrapper>
+  )
+}
+
+export default Navbar
+
+
+
