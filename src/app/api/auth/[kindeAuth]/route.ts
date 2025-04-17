4c8486c1dd04b3b61d5ad6ddaa4d4e4450480ec3
Himanshu Tiwari <htiwari578@gmail.com>
2025-04-17 23:20:13 +0530

    kinde route

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,9 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 export const GET = handleAuth();
 
-console.log("KINDE_ISSUER_URL =>", process.env.KINDE_ISSUER_URL);
+console.log("👉 KINDE_ISSUER_URL:", process.env.KINDE_ISSUER_URL); // Add this line
+
+
 
 
 
